ceab14f39d529471e618fb3e868b89f89e839cbf
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-03-29 10:19:53 +0200

    Implement message retrival and rendering

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,22 @@
-// scroll message window to the bottom
-$(".well").scrollTop($(".well")[0].scrollHeight)
+
+
+function renderMsg(messages) {
+  var template = $('#template').html();
+  Mustache.parse(template);   // optional, speeds up future uses
+  var rendered = Mustache.render(template, {messages: messages});
+  $('#message-list').html(rendered);
+
+  // scroll message window to the bottom
+  $(".well").scrollTop($(".well")[0].scrollHeight)
+}
+
+function getMessages(){
+  $.get("messages", function( data ){
+    renderMsg(data)
+    setTimeout(getMessages,5000);
+  });
+}
+
+$( document ).ready(function() {
+    getMessages();
+});
