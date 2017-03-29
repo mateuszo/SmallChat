cbf57c87f52c7207b6c198e5ea0fa7e3c793f09d
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-03-29 11:04:51 +0200

    Implement message sending using AJAX

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 
-
+// render messages using template
 function renderMsg(messages) {
   var template = $('#template').html();
   Mustache.parse(template);   // optional, speeds up future uses
@@ -10,13 +10,43 @@ function renderMsg(messages) {
   $(".well").scrollTop($(".well")[0].scrollHeight)
 }
 
+// get messages using ajax and render them
 function getMessages(){
   $.get("messages", function( data ){
     renderMsg(data)
-    setTimeout(getMessages,5000);
+    setTimeout(getMessages,2000);
+  });
+}
+
+// send POST request to add message
+function sendMessage(message){
+  $.ajax({
+    "url": "messages",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+    },
+    "success": getMessages,
+    "data": JSON.stringify(message)
   });
 }
 
 $( document ).ready(function() {
+    // poll for messages every 5s
     getMessages();
+
+    // register message form handler
+    $("#message-form").submit(function(event){
+      event.preventDefault();
+      var sender = $(this).find("#nickname").val();
+      var content = $(this).find("#message").val();
+      $(this).find("#message").val('');
+
+      var message = {
+                      "content": content,
+                      "sender": sender
+                    };
+      sendMessage(message);
+
+    });
 });
